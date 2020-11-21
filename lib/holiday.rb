b0c7df0fa76bbe34d1a233324a546b34adb2b764
creamsodajoe <velasco.jordan.r@gmail.com>
2020-11-21 14:14:55 -0600

    commit tree 4c0d23a5c7898baf5786abb21638590bbc2664e5

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,29 +1,15 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july] [1]
+
 end
 
+
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
 
 end
 
@@ -55,10 +41,10 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
 end
-
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  supplies_hash.map do |season, holidays|
+
+    holidays.map do |holiday, supplies|
 
 end
 
